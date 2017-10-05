dfd2c9587590f00cad6082ab93f7136fa111aeb4
Tivona Spruill <tivona.spruill@yahoo.com>
2017-10-05 14:45:31 -0400

    added react routes

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Dashboard from './Dashboard.jsx';
+import EventForm from './EventForm.jsx';
+import LoginPage from './LoginPage.jsx';
+import ProfilePage from './ProfilePage.jsx';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
 
       </div>
+    );
+  }
+
+  render() {
+
+  return (
+    <BrowserRouter>
+      <BaseLayout username={this.state.username} password={this.state.password} isLoggedIn={this.state.isLoggedIn} removeToken={this.removeToken.bind(this)} token={this.state.token}>
+        <Switch>
+          <Route path='/dashboard' render={Dashboard} />
+          <Route path='/event-form' render={EventForm} />
+          <Route path='/profile-page' render={ProfilePage} />
+          <Route path='/' component={LoginPage} />
+        </Switch>
+      </BaseLayout>
+    </BrowserRouter>
     );
   }
 }
