842b824c2a506bb99b5cc249c0c45987e073d739
SamSteele01 <ssteele017@gmail.com>
2017-10-05 14:54:18 -0400

    Boilerplate for LogoutButton

---
+++ src/components/LogoutButton.jsx
@@ -0,0 +1,14 @@
+import React, {PropTypes} from 'react';
+
+export default class LogoutButton extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>MyComponent</div>);
+  }
+}
+
+LogoutButton.propTypes = {
+};
