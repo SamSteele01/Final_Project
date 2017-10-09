cc1ee0d8b44790ba4f2e3b903e5dc96ad892352e
SamSteele01 <ssteele017@gmail.com>
2017-10-09 18:33:13 -0400

    Added images to be used as the icon and favicon. Made icon appear in header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, {PropTypes} from 'react';
+import Logo from '../styles/two_tickets_white1600.png'
 
 export default class Header extends React.Component {
   constructor(props) {
@@ -8,9 +9,9 @@ export default class Header extends React.Component {
   render() {
     return (
       <nav class="navbar navbar-light bg-faded">
-        <a class="navbar-brand" href="#">
-          <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt=""/>
-          <h3>EZ Tour</h3>
+        <a class="navbar-brand" >
+          <img src={Logo} width="50" height="50" class="d-inline-block " alt=""/>
+          EZ Tour
         </a>
       </nav>
     );
