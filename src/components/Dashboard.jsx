6b69de30a0b2060a5efb2f5dc1dad246b6a91af0
SamSteele01 <ssteele017@gmail.com>
2017-10-12 21:27:10 -0400

    Added navigation buttons to Dashboard. App.js now can pass props to the children. Header has conditional render of a display title, needs to be taken out of BaseLayout.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
@@ -18,7 +19,11 @@ export default class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard">
-        <h1>dashboard</h1>
+        <div className="d-flex justify-content-between">
+          <h1>Dashboard</h1>
+          <div><button className="button create-new-event-button"><Link to="/event-form">Create New Event</Link></button></div>
+          <div><button className="button create-new-event-button"><Link to="/profile-page">Edit Profile</Link></button></div>
+        </div>
         <BigCalendar
           culture='en-GB'
           events={this.state.tasks}
