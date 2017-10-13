156c7998486b387e5137d19a8f8f816d59f3bc6c
Tivona Spruill <tivona.spruill@yahoo.com>
2017-10-13 14:35:31 -0400

    added AssetToolbar component, updated ProfileEnter and added picture upload

---
+++ src/components/AssetToolbar.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class AssetToolbar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>MyComponent</div>);
+  }
+}
+AssetToolbar.propTypes = {
+};
