33805125f43e2726c5460c9f963ccad8bd9808e0
SamSteele01 <ssteele017@gmail.com>
2017-10-16 10:23:17 -0400

    Imported ProfileMini in EventVenueView.

--- src/components/EventVenueView.jsx
+++ src/components/EventVenueView.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import AssetContainer from './AssetContainer.jsx';
 import FormInput from './FormInput.jsx';
+import ProfileMini from './ProfileMini.jsx';
 
 export default class EventVenueView extends Component {
   constructor(props) {
