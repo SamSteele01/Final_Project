a9160d976c469f10b3ad5bf901bacf7f6c69d313
SamSteele01 <ssteele017@gmail.com>
2017-10-16 20:15:38 -0400

    Added function to create new calendar events. It works to create events in the array. Breaks page.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -45,12 +45,6 @@ export default class Dashboard extends Component {
         },
 
         {
-          'title': 'DTS ENDS',
-          'start': new Date(2018, 10, 6, 0, 0, 0),
-          'end': new Date(2018, 10, 13, 0, 0, 0)
-        },
-
-        {
           'title': 'All Things Open',
           'start': new Date(2017, 9, 23, 0, 0, 0),
           'end': new Date(2017, 9, 25, 0, 0, 0)
@@ -60,49 +54,6 @@ export default class Dashboard extends Component {
           'start': new Date(2017, 9, 25),
           'end': new Date(2017, 9, 25),
           desc: 'Big conference for important people'
-        },
-        {
-          'title': 'Meeting',
-          'start': new Date(2017, 3, 12, 10, 30, 0, 0),
-          'end': new Date(2017, 3, 12, 12, 30, 0, 0),
-          desc: 'Pre-meeting meeting, to prepare for the meeting'
-        },
-        {
-          'title': 'Lunch',
-          'start':new Date(2017, 3, 12, 12, 0, 0, 0),
-          'end': new Date(2017, 3, 12, 13, 0, 0, 0),
-          desc: 'Power lunch'
-        },
-        {
-          'title': 'Meeting',
-          'start':new Date(2017, 3, 12,14, 0, 0, 0),
-          'end': new Date(2017, 3, 12,15, 0, 0, 0)
-        },
-        {
-          'title': 'Happy Hour',
-          'start':new Date(2017, 3, 12, 17, 0, 0, 0),
-          'end': new Date(2017, 3, 12, 17, 30, 0, 0),
-          desc: 'Most important meal of the day'
-        },
-        {
-          'title': 'Dinner',
-          'start':new Date(2017, 3, 12, 20, 0, 0, 0),
-          'end': new Date(2017, 3, 12, 21, 0, 0, 0)
-        },
-        {
-          'title': 'Birthday Party',
-          'start':new Date(2017, 3, 13, 7, 0, 0),
-          'end': new Date(2017, 3, 13, 10, 30, 0)
-        },
-        {
-          'title': 'Late Night Event',
-          'start':new Date(2017, 3, 17, 19, 30, 0),
-          'end': new Date(2017, 3, 18, 2, 0, 0)
-        },
-        {
-          'title': 'Multi-day Event',
-          'start':new Date(2017, 3, 20, 19, 30, 0),
-          'end': new Date(2017, 3, 22, 2, 0, 0)
         }
       ]
     }
@@ -128,12 +79,12 @@ export default class Dashboard extends Component {
       });
   }
 
-  mapOverBandsArrayAndFetchEvents(arrayOfBands){
+  mapOverBandsArrayAndFetchEvents = (arrayOfBands) => {
     let events = arrayOfBands.map((band) =>{
-      console.log(band.id);
+      // console.log(band.id);
       return( this.fetchAllEventsForBand(band.id))
     })
-    this.setState({doneMapping: true});
+    console.log(this.state.eventsArray);
   }
 
   fetchAllEventsForBand(bandsId){
@@ -143,36 +94,48 @@ export default class Dashboard extends Component {
       .set('Authorization', `Token token=${this.state.token}`)
       .end((err, res) => {
         let data = res.body.events; //array
-        console.log(data);
+        // console.log(data);
         let eventsArray = this.state.eventsArray;
         let holderArray = eventsArray.concat(data);
         this.setState({eventsArray: holderArray});
       });
   }
 
-  createCalendarEvents(eventsArray){
-    let calEvents = eventsArray.map((event) =>{
-        let eventObject = {
-          'title': 'Spruill concert hall',
-          'start': new Date(2017, 9, 20, 19, 0, ),
-          'end': new Date(2017, 9, 20, 21, 30, 0)
-        }
-        let calendarEvents = this.state.calendarEvents;
-        let holderArray = calendarEvents.push(eventObject);
-        return(
-        this.setState({calendarEvents: holderArray})
-      )
+  createCalendarEvents = (arrayOfEvents) => {
+    console.log(arrayOfEvents);
+    let calEvents = arrayOfEvents.map((event) =>{
+      console.log(event);
+      return( this.createSingleEvent(event) )
     })
-    this.setState({doneMakingCalendarEvents: true});
+    console.log(this.state.calendarEvents);
+  }
+
+  createSingleEvent = (theDeets) => {
+    let eventObject = {
+      'title': theDeets.venue,
+      'start': new Date(theDeets.date),
+      'end': new Date(theDeets.date)
+    };
+    console.log(eventObject);
+    let calendarEvents = this.state.calendarEvents;
+    let holderArray = calendarEvents.push(eventObject);
+    this.setState({calendarEvents: holderArray})
+  }
+
+  formatDateAndTime(date, time){
+
+    return (2017, 9, 20, 21, 30, 0)
   }
 
   componentDidUpdate(){
-    if(this.state.doneMapping && !this.state.doneMakingCalendarEvents){
+    if(this.state.eventsArray.length>0 && this.state.doneMapping && !this.state.doneMakingCalendarEvents){
       this.createCalendarEvents(this.state.eventsArray);
+      this.setState({doneMakingCalendarEvents: true});
       // this.props.setBandList(this.state.bandsArray); redux action
     }
     if(this.state.bandsArray && !this.state.doneMapping){
       this.mapOverBandsArrayAndFetchEvents(this.state.bandsArray);
+      this.setState({doneMapping: true});
     }
   }
 
@@ -184,10 +147,12 @@ export default class Dashboard extends Component {
           <h1>Dashboard</h1>
           <div><button className="button create-new-event-button"><Link to="/profile-page">Edit Profile</Link></button></div>
         </div>
-        <BigCalendar
-          culture='en'
-          events={this.state.calendarEvents}
-          views={['month', 'week', 'day', 'agenda']}/>
+        {this.state.doneMakingCalendarEvents &&
+          <BigCalendar
+            culture='en'
+            events={this.state.calendarEvents}
+            views={['month', 'week', 'day', 'agenda']}/>
+        }
       </div>
     );
   }
