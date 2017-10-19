0712dd749b677aa6afbe5337c66c8ff64ff6c78c
SamSteele01 <ssteele017@gmail.com>
2017-10-19 15:09:48 -0400

    Created function to map over bands and populate the dropdown menu.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -71,6 +71,8 @@ export default class Dashboard extends Component {
       window.location.href = "/";
     }
     this.fetchAllEventsForUser(this.state.userId);
+    this.fetchFullnameForUser(this.state.userId);
+    this.fetchAllBandsForUser(this.state.userId);
   }
 
   fetchAllEventsForUser(userId){
@@ -80,6 +82,26 @@ export default class Dashboard extends Component {
       .end((err, res) => {
         let data = res.body.events;
         this.setState({eventsArray: data});
+      });
+  }
+
+  fetchFullnameForUser(userId){
+    request
+      .get(`https://ez-tour.herokuapp.com/users/${userId}`)
+      .set('Authorization', `Token token=${this.state.token}`)
+      .end((err, res) => {
+        let data = res.body.full_name;
+        this.setState({fullName: data});
+      });
+  }
+
+  fetchAllBandsForUser(userId){
+    request
+      .get(`https://ez-tour.herokuapp.com/users/${userId}/bands`)
+      .set('Authorization', `Token token=${this.state.token}`)
+      .end((err, res) => {
+        let data = res.body.band;
+        this.setState({bandsArray: data});
       });
   }
 
@@ -159,14 +181,32 @@ export default class Dashboard extends Component {
     window.location.href = '/event-form';
   }
 
+  displayDropdowns(){
+    if(this.state.bandsArray){
+      let bandNameDropdownItem = this.state.bandsArray.map((band) => {
+        return(
+          <DropdownItem><Link to={{ pathname: "/profile-page", state: { userProfile: false} }}>{band.name}</Link></DropdownItem>
+          // need to pass bandId and userProfile to redux
+        )
+      })
+      return(
+        <div>
+          {bandNameDropdownItem}
+        </div>
+      )
+    }else{
+      return null;
+    }
+  }
+
   render() {
     // map to create DropdownItems = user and bands - need Ids and Links
+
     return (
       <div className="dashboard">
         <div className="d-flex justify-content-between">
           <div><button className="button create-new-event-button"><Link to={{ pathname: "/event-form", state: {newEvent: true}}}>Create New Event</Link></button></div>
           <h1>Dashboard</h1>
-          <div><button className="button create-new-event-button"><Link to="/profile-page">Edit Profile</Link></button></div>
           <div>
             <div><button className="button create-new-event-button"><Link to="/event-form">Create New Band</Link></button></div>
             <Dropdown className="button create-new-event-button" isOpen={this.state.dropdownOpen} toggle={this.toggle}>
@@ -176,22 +216,22 @@ export default class Dashboard extends Component {
                <DropdownMenu>
                  {/* <DropdownItem header>Header</DropdownItem> */}
                  {/* <DropdownItem disabled>Action</DropdownItem> */}
-                 <DropdownItem><Link to={{ pathname: "/profile-page", state: { userProfile: true} }}>User Profile</Link></DropdownItem>
+                 <DropdownItem><Link to={{ pathname: "/profile-page", state: { userProfile: true} }}>{this.state.fullName}</Link></DropdownItem>
                  <DropdownItem divider />
-                 <DropdownItem><Link to={{ pathname: "/profile-page", state: { userProfile: false} }}>Band Profile</Link></DropdownItem>
+                 {this.displayDropdowns()}
                </DropdownMenu>
             </Dropdown>
           </div>
         </div>
-        {this.state.doneMakingCalendarEvents &&
-          <BigCalendar
-            selectable
-            culture='en'
-            onSelectEvent={event => this.navigateToEvent(event.bandId, event.eventId)}
-            events={this.state.calendarEvents}
-            views={['month', 'week', 'day', 'agenda']}/>
-        }
-      </div>
+      {this.state.doneMakingCalendarEvents &&
+        <BigCalendar
+          selectable
+          culture='en'
+          onSelectEvent={event => this.navigateToEvent(event.bandId, event.eventId)}
+          events={this.state.calendarEvents}
+          views={['month', 'week', 'day', 'agenda']}/>
+      }
+    </div>
     );
   }
 }
